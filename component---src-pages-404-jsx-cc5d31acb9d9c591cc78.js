c8e1170f5cea92799708eac9ec2028ea8c215daf
Keith McKnight <keith@mcknig.ht>
2019-09-28 16:43:54 -0700

    rebuild

---
+++ component---src-pages-404-jsx-cc5d31acb9d9c591cc78.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{225:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(223),o=n(222);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-cc5d31acb9d9c591cc78.js.map
